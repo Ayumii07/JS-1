dbedbf45bf84223de2f8fac2a4299a639cc062d5
Ayumii07 <ayumikhanal@gmail.com>
2026-01-03 21:44:10 +0545

    [ADD]:Operator

---
+++ operator.js
@@ -0,0 +1,25 @@
+//Q no 1 
+let a= 3, b=4;
+console.log ("Addition:", a+b);
+console.log ("Subtraction:", a-b);
+console.log ("Multiplication:", a*b);
+console.log ("Division:", a/b);
+
+//Q no 2
+let num= 6;
+console.log(num % 2 == 0?  "even": "odd");
+
+//Q no 3 
+console.log("10 > 6:", 10 > 6);
+console.log("7 == 9:", 7 == 9);
+console.log("10 <= 25:", 10 <= 25);
+
+//Q no 4
+let age=20;
+console.log ("eligible to vote:", age >=18);
+let marks=38;
+console.log ("Passed:", marks >=45);
+
+//Q no 5
+let numCheck= 6;
+console.log (numCheck >=0 ? "positive" : "negative");
