78558ca7ba9a92fc082692187bde7f90f39d361d
Ayumii07 <ayumikhanal@gmail.com>
2026-01-03 19:43:31 +0545

    [ADD]:variable

---
+++ variable.js
@@ -0,0 +1,28 @@
+//Q no 1
+let name = "Ayumi";
+let age = "20";
+let isStudent = true;
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Is student:",isStudent)
+//Q no 2
+let num = 10;
+console.log("Initial value of num:",num);
+num = 20;
+console.log("Updated value of num:",num);
+//Q no 3
+let a = 5;
+let b = 10;
+console.log("Before swap: a=",a, "b=",b);
+a = a+b;
+b = a-b;
+a = a-b;
+console.log("After swap:a=",a,"b=",b);
+//Q no 4
+const PI = 3.14;
+console.log("Value of PI:",PI);
+//PI= 3.35;//error const value cannot be reassigned
+//Q no 5
+let favoriteColor = "Blue";
+console.log("My Favorite color is",favoriteColor);
+
